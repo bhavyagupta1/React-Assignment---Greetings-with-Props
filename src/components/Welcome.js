59bc65847c053cadad0342ddf5bd93a0c00d7b4d
bhavyaupta1 <bhavyagupta8299460915@gmail.com>
2020-11-16 16:36:22 +0530

    greetings

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+export default class Welcome extends React.Component{
+    render(){
+        return (
+            <div>
+             <h1>
+             Hey {props.name}!
+             </h1>
+             <h2>Welcome to Newton School.</h2>
+            </div>
+        )
+    }
+}
